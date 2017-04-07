a9e1abff60e7e15aaf0d29e0f8827db3c76831ee
Dave King <tildedave@gmail.com>
2017-04-06 20:01:54 -0700

    board and to string funcs

--- board_test.go
+++ board_test.go
@@ -7,4 +7,29 @@ import (
 
 func TestInitialBoard(t *testing.T) {
     assert.Equal(t, pieceAtSquare(initialBoard, 0, 0), byte(0x04))
+    assert.Equal(t, pieceAtSquare(initialBoard, 0, 1), byte(0x02))
+
+    for i := 0; i < 8; i++ {
+        piece := pieceAtSquare(initialBoard, 1, byte(i))
+        assert.Equal(t, piece, byte(0x01))
+        assert.True(t, isPieceWhite(piece))
+        assert.True(t, isPawn(piece))
+    }
+    for i := 2; i <= 5; i++ {
+        for j := 0; j < 8; j++ {
+            piece := pieceAtSquare(initialBoard, byte(i), byte(j))
+            assert.Equal(t, piece, byte(0x00))
+            assert.True(t, isSquareEmpty(piece))
+        }
+    }
+    for i := 0; i < 8; i++ {
+        piece := pieceAtSquare(initialBoard, 6, byte(i))
+        assert.Equal(t, piece, byte(0x81))
+        assert.True(t, isPieceBlack(piece))
+        assert.True(t, isPawn(piece))
+    }
+}
+
+func TestToString(t *testing.T) {
+    print(boardToString(initialBoard))
 }
