0d8f58fc43cccad4d129f181a6b86bba88093dc1
Dave King <tildedave@gmail.com>
2017-04-08 10:20:15 -0700

    Add move encoding

---
+++ move.go
@@ -0,0 +1,79 @@
+package main
+
+// Move will be encoded as 32 bits - should still be fast
+// Eventually consider trying a smaller representation
+type Move struct {
+	from  uint8
+	to    uint8
+	flags uint8
+}
+
+const CAPTURE_MASK = 0x80
+const PROMOTION_MASK = 0x40 // may not be needed
+const SPECIAL1_MASK = 0x20
+const SPECIAL2_MASK = 0x10
+
+func CreateMove(from uint8, to uint8) Move {
+	var m Move
+	m.from = from
+	m.to = to
+
+	return m
+}
+
+func CreateCapture(from uint8, to uint8) Move {
+	var m Move
+	m.from = from
+	m.to = to
+	m.flags |= CAPTURE_MASK
+
+	return m
+}
+
+func CreateKingsideCastle(from uint8, to uint8) Move {
+	var m Move
+	m.from = from
+	m.to = to
+	m.flags |= SPECIAL1_MASK
+
+	return m
+}
+
+func CreateQueensideCastle(from uint8, to uint8) Move {
+	var m Move
+	m.from = from
+	m.to = to
+	m.flags |= SPECIAL1_MASK | SPECIAL2_MASK
+
+	return m
+}
+
+func CreatePromotion(from uint8, to uint8, piece uint8) Move {
+	// Piece is stored in bottom half of the promotion
+	var m Move
+	m.from = from
+	m.to = to
+	m.flags |= PROMOTION_MASK | (piece & 0x0F)
+
+	return m
+}
+
+func MoveToString(move Move) string {
+	if move.flags&SPECIAL1_MASK == SPECIAL1_MASK {
+		if move.flags&SPECIAL2_MASK == SPECIAL2_MASK {
+			return "O-O-O"
+		}
+		return "O-O"
+	}
+
+	var s string
+	s += SquareToAlgebraicString(move.from)
+	if move.flags&CAPTURE_MASK == CAPTURE_MASK {
+		s += "x"
+	} else {
+		s += "-"
+	}
+	s += SquareToAlgebraicString(move.to)
+
+	return s
+}
---
+++ move_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"unsafe"
+)
+
+var _ = fmt.Println
+
+func TestCreateMove(t *testing.T) {
+	var m = CreateMove(31, 51)
+
+	assert.Equal(t, uintptr(3), unsafe.Sizeof(m))
+	assert.Equal(t, Move{from: 31, to: 51}, m)
+}
+
+func TestCreateCapture(t *testing.T) {
+	var m = CreateCapture(31, 51)
+
+	assert.Equal(t, Move{from: 31, to: 51, flags: 0x80}, m)
+}
+
+func TestMoveToString(t *testing.T) {
+	assert.Equal(t, "a2xa4", MoveToString(CreateCapture(31, 51)))
+	assert.Equal(t, "a2-a4", MoveToString(CreateMove(31, 51)))
+	assert.Equal(t, "O-O", MoveToString(CreateKingsideCastle(25, 27)))
+	assert.Equal(t, "O-O-O", MoveToString(CreateQueensideCastle(25, 23)))
+}
