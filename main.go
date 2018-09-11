df7fa37a075187121eeedc6b2966a87b0a629f9f
Dave King <tildedave@gmail.com>
2018-09-10 22:07:22 -0400

    Add parsing logic for test position JSON

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
+
+type PerftSpecification struct {
+	Depth uint   `json:depth`
+	Nodes uint   `json:nodes`
+	Fen   string `json:fen`
+}
 
 func main() {
 	startingFen := flag.String("fen", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "Fen board")
@@ -12,6 +20,7 @@ func main() {
 	perftDepth := flag.Uint("perftdepth", 5, "Perft depth to search")
 	perftChecks := flag.Bool("countchecks", false, "Perft: count check positions (slower)")
 	perftSanityCheck := flag.Bool("sanitycheck", false, "Perft: sanity check board and moves (slower)")
+	perftJsonFile := flag.String("perftjson", "", "JSON specification")
 	perftPrintMoves := flag.Bool("printmoves", false, "Perft: print all generates moves at final depth")
 
 	flag.Parse()
@@ -21,6 +30,32 @@ func main() {
 		options.checks = *perftChecks
 		options.sanityCheck = *perftSanityCheck
 		options.perftPrintMoves = *perftPrintMoves
+
+		if *perftJsonFile != "" {
+			b, err := ioutil.ReadFile(*perftJsonFile)
+			if err != nil {
+				panic(err)
+			}
+
+			var specs []PerftSpecification
+			json.Unmarshal(b, &specs)
+
+			for _, spec := range specs {
+				board, err := CreateBoardStateFromFENString(spec.Fen)
+				if err != nil {
+					fmt.Println("Unable to parse FEN " + spec.Fen + ", continuing")
+					fmt.Println(err)
+					continue
+				}
+				perftResult := Perft(&board, spec.Depth, options)
+				if perftResult.nodes != spec.Nodes {
+					fmt.Printf("NOT OK: %s (depth=%d, nodes=%d, actual nodes=%d)\n", spec.Fen, spec.Depth, spec.Nodes, perftResult.nodes)
+				} else {
+					fmt.Printf("OK: %s (depth=%d, nodes=%d)\n", spec.Fen, spec.Depth, spec.Nodes)
+				}
+			}
+			return
+		}
 
 		for i := uint(0); i <= *perftDepth; i++ {
 			board, err := CreateBoardStateFromFENString(*startingFen)
