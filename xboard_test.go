cf0750a2bde200bb5e4eb68c41e5d542f1f84777
Dave King <tildedave@gmail.com>
2018-09-15 12:43:08 -0700

    Implement a few more commands from xboard

--- xboard_test.go
+++ xboard_test.go
@@ -13,7 +13,7 @@ func TestProcessNewCommand(t *testing.T) {
 
 	action, state := ProcessXboardCommand("new", state)
 
-	assert.Equal(t, ACTION_NOTHING, action)
+	assert.Equal(t, ACTION_HALT, action)
 	assert.Equal(t, state.boardState.ToFENString(),
 		"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 }
@@ -43,7 +43,32 @@ func TestProcessMoveCommandInForceMode(t *testing.T) {
 	assert.Equal(t, WHITE_MASK|KNIGHT_MASK, state.boardState.PieceAtSquare(SQUARE_F3))
 }
 
-func TestParseXboardCommandMove(t *testing.T) {
+func TestProcessResultCommand(t *testing.T) {
+	var state XboardState
+	var action int
+
+	_, state = ProcessXboardCommand("new", state)
+	action, state = ProcessXboardCommand("result 1/2-1/2 {Forgot how to play}", state)
+
+	assert.Equal(t, ACTION_GAME_OVER, action)
+}
+
+func TestParseXboardCommandSetboard(t *testing.T) {
+	var state XboardState
+	var action int
+
+	fenString := "r6r/1b2k1bq/8/8/7B/8/8/R3K2R b KQ - 3 2"
+	action, state = ProcessXboardCommand("setboard "+fenString, state)
+
+	assert.Equal(t, ACTION_HALT, action)
+	assert.Equal(t, fenString, state.boardState.ToFENString())
+
+	// assert.True(t, move.IsCapture())
+	// assert.Equal(t, SQUARE_E5, move.from)
+	// assert.Equal(t, SQUARE_D6, move.to)
+}
+
+func TestParseXboardMoveMove(t *testing.T) {
 	boardState := CreateInitialBoardState()
 	move, err := ParseXboardMove("e2e4", &boardState)
 
@@ -51,7 +76,7 @@ func TestParseXboardCommandMove(t *testing.T) {
 	assert.Equal(t, Move{from: SQUARE_E2, to: SQUARE_E4}, move)
 }
 
-func TestParseXboardCommandCapture(t *testing.T) {
+func TestParseXboardMoveCapture(t *testing.T) {
 	boardState := CreateEmptyBoardState()
 	boardState.SetPieceAtSquare(SQUARE_A1, ROOK_MASK|WHITE_MASK)
 	boardState.SetPieceAtSquare(SQUARE_A2, ROOK_MASK|BLACK_MASK)
@@ -62,7 +87,7 @@ func TestParseXboardCommandCapture(t *testing.T) {
 	assert.Equal(t, Move{from: SQUARE_A1, to: SQUARE_A2, flags: CAPTURE_MASK}, move)
 }
 
-func TestParseXboardCommandPromotion(t *testing.T) {
+func TestParseXboardMovePromotion(t *testing.T) {
 	boardState := CreateEmptyBoardState()
 	boardState.SetPieceAtSquare(SQUARE_H7, PAWN_MASK|WHITE_MASK)
 
@@ -75,7 +100,7 @@ func TestParseXboardCommandPromotion(t *testing.T) {
 	assert.Equal(t, move.flags, PROMOTION_MASK|QUEEN_MASK)
 }
 
-func TestParseXboardCommandPromotionCapture(t *testing.T) {
+func TestParseXboardMovePromotionCapture(t *testing.T) {
 	boardState := CreateEmptyBoardState()
 	boardState.SetPieceAtSquare(SQUARE_H7, PAWN_MASK|WHITE_MASK)
 	boardState.SetPieceAtSquare(SQUARE_G8, ROOK_MASK|BLACK_MASK)
@@ -89,7 +114,7 @@ func TestParseXboardCommandPromotionCapture(t *testing.T) {
 	assert.Equal(t, move.flags, PROMOTION_MASK|CAPTURE_MASK|ROOK_MASK)
 }
 
-func TestParseXboardCommandKingsideCastle(t *testing.T) {
+func TestParseXboardMoveKingsideCastle(t *testing.T) {
 	boardState := CreateInitialBoardState()
 	boardState.SetPieceAtSquare(SQUARE_F1, 0x00)
 	boardState.SetPieceAtSquare(SQUARE_G1, 0x00)
@@ -102,7 +127,7 @@ func TestParseXboardCommandKingsideCastle(t *testing.T) {
 	assert.Equal(t, SQUARE_G1, move.to)
 }
 
-func TestParseXboardCommandQueensideCastle(t *testing.T) {
+func TestParseXboardMoveQueensideCastle(t *testing.T) {
 	boardState := CreateInitialBoardState()
 	boardState.SetPieceAtSquare(SQUARE_B1, 0x00)
 	boardState.SetPieceAtSquare(SQUARE_C1, 0x00)
@@ -117,7 +142,7 @@ func TestParseXboardCommandQueensideCastle(t *testing.T) {
 	assert.Equal(t, SQUARE_C8, move.to)
 }
 
-func TestParseXboardCommandEnPassantCapture(t *testing.T) {
+func TestParseXboardMoveEnPassantCapture(t *testing.T) {
 	boardState := CreateEmptyBoardState()
 	boardState.SetPieceAtSquare(SQUARE_E5, WHITE_MASK|PAWN_MASK)
 	boardState.SetPieceAtSquare(SQUARE_D5, BLACK_MASK|PAWN_MASK)
