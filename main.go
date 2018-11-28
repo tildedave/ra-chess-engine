161fc0b47db3e2e6708d7c4b08e0f0577e623118
Dave King <tildedave@gmail.com>
2018-11-27 18:37:30 -0800

    Add tactics engine with BK test

--- main.go
+++ main.go
@@ -4,10 +4,20 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 )
 
 var _ = fmt.Println
+
+func InitializeLogger() {
+	file, err := os.Create("/tmp/ra-chess-engine.log")
+	if err != nil {
+		panic(err)
+	}
+	logger = log.New(file, "", log.LstdFlags|log.Lshortfile)
+	logger.Println("Starting up!")
+}
 
 func main() {
 	startingFen := flag.String("fen", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "Fen board")
@@ -15,25 +25,36 @@ func main() {
 	perftDepth := flag.Uint("perftdepth", 5, "Perft depth to search")
 	perftChecks := flag.Bool("countchecks", false, "Perft: count check positions (slower)")
 	perftSanityCheck := flag.Bool("sanitycheck", false, "Perft: sanity check board and moves (slower)")
-	perftJsonFile := flag.String("perftjson", "", "JSON specification")
+	perftJSONFile := flag.String("perftjson", "", "JSON specification")
 	perftPrintMoves := flag.Bool("printmoves", false, "Perft: print all generates moves at final depth")
+	isTactics := flag.Bool("tactics", false, "Tactics mode")
+	tacticsEpdFile := flag.String("tacticsepd", "", "Tactics file in EPD format")
+	tacticsThinkingTime := flag.Uint("tacticsthinkingtime", 500, "Time to think per position (ms)")
+	tacticsRegex := flag.String("tacticsregex", "", "Run only tactics matching the given id")
 
 	flag.Parse()
 
-	var success bool = true
-	var err error = nil
+	InitializeLogger()
+	var success = true
+	var err error
 
-	if *isPerft || *perftJsonFile != "" {
+	if *isPerft || *perftJSONFile != "" {
 		var options PerftOptions
 		options.checks = *perftChecks
 		options.sanityCheck = *perftSanityCheck
 		options.perftPrintMoves = *perftPrintMoves
 
-		if *perftJsonFile != "" {
-			success, err = RunPerftJson(*perftJsonFile, options)
+		if *perftJSONFile != "" {
+			success, err = RunPerftJson(*perftJSONFile, options)
 		} else {
 			success, err = RunPerft(*startingFen, *perftDepth, options)
 		}
+	} else if *isTactics || *tacticsEpdFile != "" {
+		var options TacticsOptions
+		options.thinkingtimeMs = *tacticsThinkingTime
+		options.tacticsregex = *tacticsRegex
+
+		success, err = RunTacticsFile(*tacticsEpdFile, options)
 	} else {
 		// xboard mode
 		scanner := bufio.NewScanner(os.Stdin)
