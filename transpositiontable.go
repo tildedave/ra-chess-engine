d5bf324b947a84c480265dffd9e18369dc2d753f
Dave King <tildedave@gmail.com>
2018-12-02 09:57:44 -0800

    Oh yes, we need to add the transposition table file

---
+++ transpositiontable.go
@@ -0,0 +1,25 @@
+package main
+
+type TranspositionEntry struct {
+	depth  uint
+	result *SearchResult
+}
+
+var hashArray map[uint32]*TranspositionEntry
+
+func generateTranspositionTable(boardState *BoardState) {
+	boardState.transpositionTable = make(map[uint32]*TranspositionEntry)
+}
+
+func ProbeTranspositionTable(boardState *BoardState) *TranspositionEntry {
+	return boardState.transpositionTable[boardState.hashKey]
+}
+
+func StoreTranspositionTable(boardState *BoardState, result *SearchResult, depth uint) {
+	entry := TranspositionEntry{result: result, depth: depth}
+	// TODO: don't store if depth is lower than depth in table
+	e := boardState.transpositionTable[boardState.hashKey]
+	if e == nil || e.depth < depth {
+		boardState.transpositionTable[boardState.hashKey] = &entry
+	}
+}
