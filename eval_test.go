cc3c6ce5ce45ed926f008c85857078ed3a840edf
Dave King <tildedave@gmail.com>
2018-12-21 13:03:38 -0800

    Eval: move hasMatingMaterial to struct

--- eval_test.go
+++ eval_test.go
@@ -11,7 +11,7 @@ var _ = fmt.Println
 
 func TestEvalEmptyBoard(t *testing.T) {
 	testBoard := CreateEmptyBoardState()
-	boardEval, _ := Eval(&testBoard)
+	boardEval := Eval(&testBoard)
 
 	assert.Equal(t, 0, boardEval.material)
 }
@@ -20,7 +20,7 @@ func TestEvalPawn(t *testing.T) {
 	testBoard := CreateEmptyBoardState()
 	testBoard.SetPieceAtSquare(SQUARE_A2, WHITE_MASK|PAWN_MASK)
 
-	boardEval, _ := Eval(&testBoard)
+	boardEval := Eval(&testBoard)
 
 	assert.Equal(t, 100, boardEval.material)
 }
@@ -30,7 +30,7 @@ func TestEvalPawnAgainstBishop(t *testing.T) {
 	testBoard.SetPieceAtSquare(SQUARE_A2, WHITE_MASK|PAWN_MASK)
 	testBoard.SetPieceAtSquare(SQUARE_A3, BLACK_MASK|BISHOP_MASK)
 
-	boardEval, _ := Eval(&testBoard)
+	boardEval := Eval(&testBoard)
 
 	assert.Equal(t, -200, boardEval.material)
 }
@@ -39,13 +39,13 @@ func TestEvalPassedPawns(t *testing.T) {
 	testBoard := CreateEmptyBoardState()
 	testBoard.SetPieceAtSquare(SQUARE_A2, WHITE_MASK|PAWN_MASK)
 
-	boardEval, _ := Eval(&testBoard)
+	boardEval := Eval(&testBoard)
 	fmt.Println(boardEval)
 }
 
 func TestEvalStartingPosition(t *testing.T) {
 	testBoard := CreateInitialBoardState()
-	boardEval, _ := Eval(&testBoard)
+	boardEval := Eval(&testBoard)
 
 	assert.Equal(t, boardEval.material, 0)
 }
@@ -54,7 +54,7 @@ func TestEvalStartingPositionCenterControl(t *testing.T) {
 	testBoard := CreateInitialBoardState()
 	testBoard.SetPieceAtSquare(SQUARE_E2, 0x00)
 	testBoard.SetPieceAtSquare(SQUARE_E4, WHITE_MASK|PAWN_MASK)
-	boardEval, _ := Eval(&testBoard)
+	boardEval := Eval(&testBoard)
 
 	assert.Equal(t, boardEval.material, 0)
 }
@@ -66,7 +66,7 @@ func TestEvalKingSafety(t *testing.T) {
 	testBoard.SetPieceAtSquare(SQUARE_F1, WHITE_MASK|ROOK_MASK)
 	testBoard.SetPieceAtSquare(SQUARE_G1, WHITE_MASK|KING_MASK)
 
-	boardEval, _ := Eval(&testBoard)
+	boardEval := Eval(&testBoard)
 
 	assert.Equal(t, boardEval.kingPosition, KING_PAWN_COVER_EVAL_SCORE*3+KING_IN_CENTER_EVAL_SCORE)
 }
