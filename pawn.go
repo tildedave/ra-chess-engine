0ef2492234a3c74b3abf48001bc73b352529fac1
Dave King <tildedave@gmail.com>
2020-08-23 14:31:11 -0700

    Pawns: compute isolated and connected pawns

--- pawn.go
+++ pawn.go
@@ -5,9 +5,12 @@ import (
 )
 
 type PawnTableEntry struct {
-	passedPawns  [2]uint64
-	doubledPawns [2]uint64
-	pawnsPerRank [2][8]uint64
+	pawns          [2]uint64
+	passedPawns    [2]uint64
+	doubledPawns   [2]uint64
+	isolatedPawns  [2]uint64
+	connectedPawns [2]uint64
+	pawnsPerRank   [2][8]uint64
 }
 
 // Given a bitboard which is the pawns for a single side, return a bitboard which
@@ -90,6 +93,33 @@ func GetPawnRankBitboard(pawnBitboard uint64, rank byte) uint64 {
 	return pawnBitboard & bitboard
 }
 
+func GetIsolatedPawnBitboard(pawnBitboard uint64) uint64 {
+	var bitboard uint64
+	originalBoard := pawnBitboard
+	for pawnBitboard != 0 {
+		sq := byte(bits.TrailingZeros64(pawnBitboard))
+		pawnBitboard ^= 1 << sq
+
+		col := sq % 8
+		var columnBoard uint64
+		for j := byte(1); j < 7; j++ {
+			maskSq := idx(col, j)
+			if col > 0 {
+				columnBoard = SetBitboard(columnBoard, maskSq-1)
+			}
+			if col < 7 {
+				columnBoard = SetBitboard(columnBoard, maskSq+1)
+			}
+		}
+
+		if columnBoard&originalBoard == 0 {
+			bitboard = SetBitboard(bitboard, sq)
+		}
+	}
+
+	return bitboard
+}
+
 // Return the pawn table entry for the given board state
 func GetPawnTableEntry(boardState *BoardState) *PawnTableEntry {
 	tableEntry := boardState.pawnTable[boardState.pawnHashKey]
@@ -102,6 +132,8 @@ func GetPawnTableEntry(boardState *BoardState) *PawnTableEntry {
 	allPawns := boardState.bitboards.piece[PAWN_MASK]
 	whitePawns := allPawns & boardState.bitboards.color[WHITE_OFFSET]
 	blackPawns := allPawns & boardState.bitboards.color[BLACK_OFFSET]
+	entry.pawns[WHITE_OFFSET] = whitePawns
+	entry.pawns[BLACK_OFFSET] = blackPawns
 	entry.doubledPawns[WHITE_OFFSET] = GetDoubledPawnBitboard(whitePawns)
 	entry.doubledPawns[BLACK_OFFSET] = GetDoubledPawnBitboard(blackPawns)
 	entry.passedPawns[WHITE_OFFSET] = GetPassedPawnBitboard(whitePawns, blackPawns, WHITE_OFFSET)
@@ -110,6 +142,10 @@ func GetPawnTableEntry(boardState *BoardState) *PawnTableEntry {
 		entry.pawnsPerRank[WHITE_OFFSET][rank] = GetPawnRankBitboard(whitePawns, rank)
 		entry.pawnsPerRank[BLACK_OFFSET][rank] = GetPawnRankBitboard(blackPawns, rank)
 	}
+	entry.isolatedPawns[WHITE_OFFSET] = GetIsolatedPawnBitboard(whitePawns)
+	entry.isolatedPawns[BLACK_OFFSET] = GetIsolatedPawnBitboard(blackPawns)
+	entry.connectedPawns[WHITE_OFFSET] = whitePawns ^ entry.isolatedPawns[WHITE_OFFSET]
+	entry.connectedPawns[BLACK_OFFSET] = blackPawns ^ entry.isolatedPawns[BLACK_OFFSET]
 	boardState.pawnTable[boardState.pawnHashKey] = &entry
 
 	return &entry
