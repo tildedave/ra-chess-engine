ed3494a2ac8db7aab76faca7802b0e856838fc00
Dave King <tildedave@gmail.com>
2020-08-23 11:09:27 -0700

    Pawns: add passed pawn bitboard calculation

--- pawn_test.go
+++ pawn_test.go
@@ -16,6 +16,21 @@ func TestDoubledPawnBitboard(t *testing.T) {
 	bitboard = SetBitboard(SetBitboard(0, SQUARE_D4), SQUARE_D3)
 	assert.Equal(t, bitboard, GetDoubledPawnBitboard(bitboard))
 
-	bitboard = SetBitboard(SetBitboard(SetBitboard(0, SQUARE_A4), SQUARE_A3), A2)
+	bitboard = SetBitboard(SetBitboard(SetBitboard(0, SQUARE_A4), SQUARE_A3), SQUARE_A2)
 	assert.Equal(t, bitboard, GetDoubledPawnBitboard(bitboard))
+}
+
+func TestPassedPawnBitboard(t *testing.T) {
+	bitboard := SetBitboard(0, SQUARE_D4)
+	otherBitboard := SetBitboard(0, SQUARE_D7)
+
+	assert.Equal(t, uint64(0), GetPassedPawnBitboard(bitboard, otherBitboard, WHITE_OFFSET))
+	assert.Equal(t, uint64(0), GetPassedPawnBitboard(otherBitboard, bitboard, BLACK_OFFSET))
+	assert.Equal(t, bitboard, GetPassedPawnBitboard(bitboard, 0, WHITE_OFFSET))
+
+	bitboard = SetBitboard(0, SQUARE_A2)
+	otherBitboard = SetBitboard(0, SQUARE_H4)
+
+	assert.Equal(t, bitboard, GetPassedPawnBitboard(bitboard, otherBitboard, WHITE_OFFSET))
+	assert.Equal(t, otherBitboard, GetPassedPawnBitboard(otherBitboard, bitboard, BLACK_OFFSET))
 }
