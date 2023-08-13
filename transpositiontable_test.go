5263529e183083e136d2aa9a2fb1f56bb4ebdb9a
Dave King <tildedave@gmail.com>
2023-08-12 21:48:30 -0400

    Transposition Table: Fix cool 2s-complement bug
    Casting a negative int16 to a uint64 gives leading 0xFs; we only want the first 24 of them for the score and the first 8 of them for the depth.

--- transpositiontable_test.go
+++ transpositiontable_test.go
@@ -12,9 +12,15 @@ func TestStoreAndProbe(t *testing.T) {
 	assert.Nil(t, ProbeTranspositionTable(&boardState))
 	move := Move{from: SQUARE_A1, to: SQUARE_H4, flags: CAPTURE_MASK}
 	StoreTranspositionTable(&boardState, move, 14_000, TT_EXACT, 32)
-	entry := ProbeTranspositionTable(&boardState)
 
 	assert.Equal(t,
 		TranspositionEntry{move: move, depth: 32, entryType: TT_EXACT, score: 14_000},
-		*entry)
+		*ProbeTranspositionTable(&boardState))
+
+	move = Move{from: SQUARE_B3, to: SQUARE_A8, flags: PROMOTION_MASK | CAPTURE_MASK}
+	StoreTranspositionTable(&boardState, move, -32, TT_FAIL_HIGH, -10)
+
+	assert.Equal(t,
+		TranspositionEntry{move: move, depth: -10, entryType: TT_FAIL_HIGH, score: -32},
+		*ProbeTranspositionTable(&boardState))
 }
